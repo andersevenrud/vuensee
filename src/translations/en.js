683e3f9689fb1aceabcc5b23be8c972f4c809759
Anders Evenrud <andersevenrud@gmail.com>
2020-12-13 02:53:36 +0100

    Added Norwegian translations (#4)

---
+++ src/translations/en.js
@@ -0,0 +1,86 @@
+/*
+ * vuensee
+ * @author Anders Evenrud <andersevenrud@gmail.com>
+ * @license MIT
+ */
+
+export default {
+  controls: {
+    settings: 'Toggle settings',
+    keys: 'Send keys',
+    clipboard: 'Toggle clipboard',
+    fullscreen: 'Toggle fullscreen',
+    power: 'Toggle power controls',
+    connect: 'Connect',
+    disconnect: 'Disconnect',
+    drag: 'Drag Viewport',
+    touchKeyboard: 'Touch device keyboard'
+  },
+
+  keys: {
+    esc: 'Esc',
+    ctrl: 'Ctrl',
+    alt: 'Alt',
+    windows: 'Windows',
+    tab: 'Tab',
+    cad: 'Ctrl+Alt+Del'
+  },
+
+  clipboard: {
+    clear: 'Clear'
+  },
+
+  power: {
+    shutdown: 'Shutdown',
+    reboot: 'Reboot',
+    reset: 'Reset'
+  },
+
+  scalingModes: {
+    off: 'None',
+    scale: 'Local scaling',
+    remote: 'Remote scaling'
+  },
+
+  settings: {
+    session: {
+      header: 'Session',
+      sharedMode: 'Shared Mode',
+      viewOnly: 'View Only',
+      bell: 'System bell sound',
+      dotCursor: 'Show dot when no cursor',
+      clipToWindow: 'Clip to Window'
+    },
+    visuals: {
+      header: 'Visuals',
+      scalingMode: 'Scaling Mode',
+      quality: 'Quality',
+      compression: 'Compression level'
+    },
+    connection: {
+      header: 'Connection',
+      repeaterId: 'Repeater ID',
+      hostname: 'Hostname',
+      port: 'Port',
+      path: 'Path',
+      password: 'Passsword',
+      ssl: 'Secure socket',
+      reconnect: 'Automatic Reconnect',
+      reconnectDelay: 'Reconnection Delay (ms)'
+    }
+  },
+
+  login: {
+    passwordPlaceholder: 'Enter account password',
+    submit: 'Submit'
+  },
+
+  messages: {
+    connecting: 'Connecting',
+    disconnected: 'Disconnected',
+    reconnecting: 'Reconnecting in {delay}ms...',
+    connectionLost: 'Connection was lost',
+    connectionFailure: 'Could not establish connection',
+    securityFailure: 'Security handshake failure ({status}): {reason}'
+  }
+}
---
+++ src/translations/no.js
@@ -0,0 +1,86 @@
+/*
+ * vuensee
+ * @author Anders Evenrud <andersevenrud@gmail.com>
+ * @license MIT
+ */
+
+export default {
+  controls: {
+    settings: 'Instillinger',
+    keys: 'Send tastetrykk',
+    clipboard: 'Utklippstavle',
+    fullscreen: 'Fullskjerm',
+    power: 'Strøkkontrol',
+    connect: 'Koble til',
+    disconnect: 'Koble fra',
+    drag: 'Beveg visningsramme',
+    touchKeyboard: 'Touch-enhet tastatur'
+  },
+
+  keys: {
+    esc: 'Esc',
+    ctrl: 'Ctrl',
+    alt: 'Alt',
+    windows: 'Windows',
+    tab: 'Tab',
+    cad: 'Ctrl+Alt+Del'
+  },
+
+  clipboard: {
+    clear: 'Tøm'
+  },
+
+  power: {
+    shutdown: 'Slå av',
+    reboot: 'Omstart',
+    reset: 'Reset'
+  },
+
+  scalingModes: {
+    off: 'Ingen',
+    scale: 'Lokal skalering',
+    remote: 'Fjern skalering'
+  },
+
+  settings: {
+    session: {
+      header: 'Sessjon',
+      sharedMode: 'Delt Modus',
+      viewOnly: 'Kun Visning',
+      bell: 'Bjellelyd fra system',
+      dotCursor: 'Vis dott hvis ingen peker',
+      clipToWindow: 'Klipp til vindu'
+    },
+    visuals: {
+      header: 'Visuelt',
+      scalingMode: 'Skaleringsmodus',
+      quality: 'Kvalitet',
+      compression: 'Kompressjonsnivå'
+    },
+    connection: {
+      header: 'Tilkobling',
+      repeaterId: 'Repeater ID',
+      hostname: 'Vertsnavn',
+      port: 'Port',
+      path: 'Sti',
+      password: 'Passord',
+      ssl: 'Sikker socket',
+      reconnect: 'Automatisk gjenkobling',
+      reconnectDelay: 'Gjenkoblings forsinkelse (ms)'
+    }
+  },
+
+  login: {
+    passwordPlaceholder: 'Tast inn kontopassord',
+    submit: 'Send'
+  },
+
+  messages: {
+    connecting: 'Kobler til',
+    disconnected: 'Koglet fra',
+    reconnecting: 'Gjenkobler om {delay}ms...',
+    connectionLost: 'Kobling ble brutt',
+    connectionFailure: 'Kunne ikke opprette kobling',
+    securityFailure: 'Sikkerhets håndtrykk feilet ({status}): {reason}'
+  }
+}
