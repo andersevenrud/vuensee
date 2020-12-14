85d1a77d8a70c08f62fa9cdf4f8f2c8410cf95fd
Anders Evenrud <andersevenrud@gmail.com>
2020-12-14 20:45:36 +0100

    Nicer ranged input stylings (#1)

--- src/components/inputs/Range.vue
+++ src/components/inputs/Range.vue
@@ -17,90 +17,65 @@
   </div>
 </template>
 
-<style module>
+<style module lang="scss">
+@mixin slider {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  width: 1rem;
+  height: 2rem;
+  height: 2.3rem;
+  background-color: currentColor;
+  cursor: pointer;
+  border-radius: 0;
+}
+
+@mixin track {
+  width: 100%;
+  height: 1rem;
+  border: var(--vuensee-input-border);
+  background-color: var(--vuensee-input-background-color);
+  cursor: pointer;
+}
+
 .range label span {
   display: block;
   margin-bottom: var(--vuensee-margin-quarter);
 }
 
-/* NOTE: It's not possible to use multiple pseudo definitions with comma
- separation :( So for now this is this way */
-
-/* RESET */
 .range input {
+  /* Reset */
+  -webkit-appearance: none;
+  -moz-appearance: none;
   background: transparent;
   display: block;
   width: 100%;
-  height: 2rem;
-  -webkit-appearance: none;
-  -moz-appearance: none;
+  height: 2.375rem;
 }
 
 .range input[type=range]::-webkit-slider-thumb {
-  -webkit-appearance: none;
+  @include slider();
+
+  margin-top: -0.75rem;
+}
+.range input[type=range]::-moz-range-thumb {
+  @include slider();
+}
+.range input[type=range]::-ms-thumb {
+  @include slider();
 }
 
+.range input[type=range]::-webkit-slider-runnable-track {
+  @include track();
+}
+.range input[type=range]::-moz-range-track {
+  @include track();
+}
 .range input[type=range]::-ms-track {
-  width: 100%;
-  cursor: pointer;
+  @include track();
+
   background: transparent;
   border-color: transparent;
   color: transparent;
-}
-
-/* SLIDER */
-.range input[type=range]::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  margin-top: 3px;
-  width: calc(2rem - 10px);
-  height: calc(2rem - 10px);
-  background-color: currentColor;
-  cursor: pointer;
-}
-
-.range input[type=range]::-moz-range-thumb {
-  -moz-appearance: none;
-  margin-top: 3px;
-  width: calc(2rem - 10px);
-  height: calc(2rem - 10px);
-  background-color: currentColor;
-  cursor: pointer;
-  border-radius: 0;
-  border: 0 none;
-}
-
-.range input[type=range]::-ms-thumb {
-  -webkit-appearance: none;
-  margin-top: 3px;
-  width: calc(2rem - 10px);
-  height: calc(2rem - 10px);
-  background-color: currentColor;
-  cursor: pointer;
-}
-
-/* TRACK */
-.range input[type=range]::-webkit-slider-runnable-track {
-  width: 100%;
-  height: 2rem;
-  border: var(--vuensee-input-border);
-  background-color: var(--vuensee-input-background-color);
-  cursor: pointer;
-}
-
-.range input[type=range]::-moz-range-track {
-  width: 100%;
-  height: 1rem;
-  border: var(--vuensee-input-border);
-  background-color: var(--vuensee-input-background-color);
-  cursor: pointer;
-}
-
-.range input[type=range]::-ms-track {
-  width: 100%;
-  height: 1rem;
-  border: var(--vuensee-input-border);
-  background-color: var(--vuensee-input-background-color);
-  cursor: pointer;
 }
 </style>
 
